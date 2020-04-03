b3e623639e18274e8c4cdaead4dff181c86baa78
Phan Minh Vũ <phanminhvuit@gmail.com>
2020-04-03 18:28:10 +0700

    update redirect path signup

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -25,9 +25,14 @@ class Users::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  private 
+  private
 
   def after_sign_in_path_for(resource)
+    return '' if resource.invalid?
+    admin_admin_index_path
+  end
+
+  def after_sign_up_path_for(resource)
     return '' if resource.invalid?
     admin_admin_index_path
   end

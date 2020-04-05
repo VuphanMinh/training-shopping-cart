03169997d826e62e476f3da48d25a1c2e9423e8b
Phan Minh Vũ <phanminhvuit@gmail.com>
2020-04-05 15:41:00 +0700

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# PRODUCT CATEGORY
+User.destroy_all
+Customer.destroy_all
 ProductCat.destroy_all
+ProductDetail.destroy_all
+ProductImage.destroy_all
+
+# PRODUCT CATEGORY
 productcat1 = ProductCat.create({:id=>100, :name=>"Tea", :typeProduct=>"drink", :description=>"testing..."})
 productcat2 = ProductCat.create({:id=>102, :name=>"Milk Tea", :typeProduct=>"drink", :description=>"testing..."})
 productcat3 = ProductCat.create({:id=>103, :name=>"Fresh Drink", :typeProduct=>"drink", :description=>"testing..."})
 
 # PRODUCT DETAIL
-ProductDetail.destroy_all
 product1 = ProductDetail.create({:id=>100, :name=>"toocha 01", :description => "testing...", :details => "testing...", :price=>10000, :discount=>0, :product_cat_id=>100})
 product1 = ProductDetail.create({:id=>101, :name=>"toocha 02", :description => "testing...", :details => "testing...", :price=>12000, :discount=>0, :product_cat_id=>100})
 product1 = ProductDetail.create({:id=>102, :name=>"toocha 03", :description => "testing...", :details => "testing...", :price=>14000, :discount=>0, :product_cat_id=>102})
@@ -21,9 +25,8 @@ product1 = ProductDetail.create({:id=>103, :name=>"toocha 04", :description => "
 product1 = ProductDetail.create({:id=>104, :name=>"toocha 05", :description => "testing...", :details => "testing...", :price=>20000, :discount=>0, :product_cat_id=>102})
 
 # PRODUCT IMAGES
-ProductImage.destroy_all
-img1 = ProductImage.create({:id=>100, :product_detail_id=>100, :product_type => "product_detail", :url_img => "1.jpg"})
-img2 = ProductImage.create({:id=>101, :product_detail_id=>101, :product_type => "product_detail", :url_img => "2.jpg"})
-img3 = ProductImage.create({:id=>102, :product_detail_id=>102, :product_type => "product_detail", :url_img => "3.jpg"})
-img4 = ProductImage.create({:id=>103, :product_detail_id=>103, :product_type => "product_detail", :url_img => "4.jpg"})
-img5 = ProductImage.create({:id=>104, :product_detail_id=>104, :product_type => "product_detail", :url_img => "5.jpg"})
+img1 = ProductImage.create({:id=>100, :product_detail_id=>100, :product_type => "product_detail", :url_img => "100.jpg"})
+img2 = ProductImage.create({:id=>101, :product_detail_id=>101, :product_type => "product_detail", :url_img => "101.jpg"})
+img3 = ProductImage.create({:id=>102, :product_detail_id=>102, :product_type => "product_detail", :url_img => "102.jpg"})
+img4 = ProductImage.create({:id=>103, :product_detail_id=>103, :product_type => "product_detail", :url_img => "103.jpg"})
+img5 = ProductImage.create({:id=>104, :product_detail_id=>104, :product_type => "product_detail", :url_img => "104.jpg"})
